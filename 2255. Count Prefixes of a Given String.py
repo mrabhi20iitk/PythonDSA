b3258ee2e00e5352009945e6fd745029ce30b6df
mrabhi20iitk <mrabhi20iitk@gmail.com>
2024-01-07 20:11:06 +0000

    Leetcode 2255

--- 2255. Count Prefixes of a Given String.py
+++ 2255. Count Prefixes of a Given String.py
@@ -1,3 +1,14 @@
+#Solution 1 : List comprehension :
+```
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        return sum([1 for i in words if s[:len(i)] == i])
+        
+```
+
+
+#Solution  2  : Using dictionary
+```
 class Solution:
     def countPrefixes(self, words: List[str], s: str) -> int:
         m = {}
@@ -12,3 +23,18 @@ class Solution:
                 continue
         
         return count
+        
+```
+
+#Solution 3 : For loop with list slicing:
+```
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        count = 0
+        for i in words:
+            if (s[:len(i)]) == i:
+                count+=1
+                
+        return count
+```
+
